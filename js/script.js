fef78191a323637f10903ddfb01854659d2f38a2
Hubert Pol <hubertpol@wp.pl>
2019-10-09 11:36:21 +0200

    first variable

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function printMessage(msg){
-    var div = document.createElement('div');
-    div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
-}
+let playerMove = `papier`;
 
-function clearMessages(){
-    document.getElementById('messages').innerHTML = '';
-}
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
