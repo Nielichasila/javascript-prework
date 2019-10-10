62d3e54d04e0781414c384a08b70062d1568df88
Hubert Pol <hubertpol@wp.pl>
2019-10-10 23:20:22 +0200

    add js buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+function playGame(playerInput){
+    
+clearMessages();
+    
+    
+    
+    
 function getMoveName(argMoveId){
     if(argMoveId == 1){
         return 'kamień';
@@ -30,7 +37,7 @@ let computerMove = getMoveName(randomNumber);
 
 printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+/*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
 
 console.log('Gracz wpisał: ' + playerInput);
 
@@ -63,5 +70,14 @@ function displayResult (argComputerMove, argPlayerMove) {
 
 displayResult(computerMove, playerMove);
 
-    
+}
 
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame('1');
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame('2');
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame('3');
+});
